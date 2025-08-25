5cf4ee7bf5464844e64fe689869d5180e8454491
Redeven <18680184+redeven@users.noreply.github.com>
2025-08-24 21:23:08 -0300

    Only replace backticked times

--- ReplaceTimestamps/index.jsx
+++ ReplaceTimestamps/index.jsx
@@ -6,9 +6,6 @@ import React from "react";
 import showChangelog from "../common/Changelog";
 import SettingsPanel from "./components/settings";
 import Settings from "./modules/settings";
-import { getRelativeTime, getUnixTimestamp } from "./modules/utils";
-
-export let timeRegexMatch, dateRegexMatch, relativeRegexMatch;
 
 export default class ReplaceTimestamps {
     start() {
@@ -24,30 +21,16 @@ export default class ReplaceTimestamps {
     patchMessageActions() {
         const MessageActions = Webpack.getByKeys("sendMessage", "editMessage");
 
-        const timeRegex = /(?<!\d)\d{1,2}:\d{2}(?!\d)(am|pm)?/gi;
-        timeRegexMatch = /((?<!\d)\d{1,2}:\d{2}(?!\d))(am|pm)?/i;
-
-        const dateFormat = Settings.get("dateFormat", "dd.MM.yyyy")
-            .replace(/[./]/g, "[./]")
-            .replace("dd", "(\\d{2})")
-            .replace("MM", "(\\d{2})")
-            .replace("yyyy", "(\\d{4})");
-
-        const dateRegex = new RegExp(`${dateFormat}`, "gi");
-        dateRegexMatch = new RegExp(`${dateFormat}`, "i");
-
-        const TimeDateRegex = new RegExp(`(${timeRegex.source})\\s+${dateRegex.source}`, "gi");
-        const DateRegexTime = new RegExp(`${dateRegex.source}\\s+(${timeRegex.source})`, "gi");
-
-        const relativeRegex = /\b(?:in\s+(\d+)([smhdw]|mo|y)|(\d+)([smhdw]|mo|y)\s+ago)\b/gi;
-        relativeRegexMatch = /\b(?:in\s+(\d+)([smhdw]|mo|y)|(\d+)([smhdw]|mo|y)\s+ago)\b/i;
+        function parseTime(time) {
+            const cleanTime = time.slice(1, -1).replace(/(\d)(AM|PM)$/i, "$1 $2");
+            let ms = new Date(`${new Date().toDateString()} ${cleanTime}`).getTime() / 1000;
+            if (isNaN(ms)) return time;
+            if (Date.now() / 1000 > ms) ms += 86400;
+            return `<t:${Math.round(ms)}:t>`;
+        }
 
         const processMessageContent = content => content
-            .replace(TimeDateRegex, x => getUnixTimestamp(x))
-            .replace(DateRegexTime, x => getUnixTimestamp(x))
-            .replace(timeRegex, x => getUnixTimestamp(x, "t"))
-            .replace(dateRegex, x => getUnixTimestamp(x, "d"))
-            .replace(relativeRegex, getRelativeTime);
+            .replace(/`\d{1,2}:\d{2} ?(?:AM|PM)?`/gi, parseTime);
 
         Patcher.before(MessageActions, "sendMessage", (_, [, msg]) => {
             msg.content = processMessageContent(msg.content);
